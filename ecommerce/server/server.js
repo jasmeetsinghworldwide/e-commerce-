b4b92d8502219065ecd8550fc86421e38be088d5
Goutamsingla01 <122777044+Goutamsingla01@users.noreply.github.com>
2025-05-07 23:17:03 +0530

    env backenf url

--- ecommerce/server/server.js
+++ ecommerce/server/server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: `${import.meta.env.VITE_API_FRONTEND_URL}`,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
